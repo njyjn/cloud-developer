fb87a5450029287ef30d6f9cd5300f2b26eec522
Justin Ng <justin@aletheon.co>
2021-02-27 18:01:53 -0800

    Fix filterImageFromURL to handle rejects

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,7 @@
+import { reject } from 'bluebird';
 import fs from 'fs';
 import Jimp = require('jimp');
+import { nextTick } from 'process';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,16 +11,22 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
+    return new Promise( async (resolve, reject) => {
         const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+        await Jimp.read(inputURL)
+            .then((photo) => {
+                photo
+                    .resize(256, 256) // resize
+                    .quality(60) // set JPEG quality
+                    .greyscale() // set greyscale
+                    .write(__dirname+outpath, (img)=>{
+                        resolve(__dirname+outpath);
+                    });
+    
+            })
+            .catch((error) => {
+                return reject(error.toString())
+        })
     });
 }
 
